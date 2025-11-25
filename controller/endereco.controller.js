12773fab83c0b83b085dad2c0970beaa39f0adf8
Mateus Queiroz <mateus_logrado@estudante.sesisenai.org.br>
2025-11-25 07:31:54 -0300

    controller endereco

--- controller/endereco.controller.js
+++ controller/endereco.controller.js
@@ -19,7 +19,7 @@ const cadastrar = async (req,res)=>{
                 uf: body.uf,
                 numero: body.numero,
                 apelido: body.apelido,
-                is_principal: is_principal
+                is_principal: body.is_principal
             }
 
             await Endereco.create(valores)
@@ -30,3 +30,67 @@ const cadastrar = async (req,res)=>{
         console.error("Erro ao cadastrar o endereço",err)
     }
 }
+
+const listar = async (req,res)=>{
+    try{
+        const enderecos = await Endereco.findAll()
+        res.status(200).json(enderecos)
+    }catch(err){
+        res.status(500).json({error: "Erro ao listar o endereço"})
+        console.error("Erro ao listar o endereço",err)
+    }
+}
+
+const atualizar = async (req,res)=>{
+    const body = req.body
+    const id = req.params.id
+
+    try{
+        const endereco = await Endereco.findByPk(id)
+        const usuario = await Usuario.findOne({where: {email: body.email}})
+        
+        if(!endereco){
+            return res.status(404).json({message: "Endereço não encontrado"})
+        }else{
+            const valores = {
+                idUsuario: usuario.codUsuario,
+                cep: body.cep,
+                logradouro: body.logradouro,
+                complemento: body.complemento,
+                bairro: body.bairro,
+                localidade: body.localidade,
+                uf: body.uf,
+                numero: body.numero,
+                apelido: body.apelido,
+                is_principal: body.is_principal
+            }
+
+
+            await Endereco.update(valores, {where: {codEndereco: id}})
+            res.status(200).json({message: "Ednereço atualizado com sucesso"})
+        }
+    }catch(err){
+        res.status(500).json({error: "Erro ao atualizar o endereço"})
+        console.error("Erro ao atualizar o endereço",err)
+    }
+}
+
+const apagar = async (req,res)=>{
+    const id = req.params.id
+
+    try{
+        const endereco = await Endereco.findByPk(id)
+        
+        if(!endereco){
+            return res.status(404).json({message: "Endereço não encontrado"})
+        }else{
+            await Endereco.destroy({where: {codEndereco: id}})
+            res.status(200).json({message: "Ednereço apagado com sucesso"})
+        }
+    }catch(err){
+        res.status(500).json({error: "Erro ao atualizar o endereço"})
+        console.error("Erro ao atualizar o endereço",err)
+    }
+}
+
+module.exports = { cadastrar, listar, atualizar, apagar }
