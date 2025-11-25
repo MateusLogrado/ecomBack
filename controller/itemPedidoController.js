0b27af6f08d0e39ca7381f8529f04cb6bc68a1b7
Mateus Queiroz <mateus_logrado@estudante.sesisenai.org.br>
2025-11-25 08:15:09 -0300

    controller itemPedido

---
+++ controller/itemPedidoController.js
@@ -0,0 +1,14 @@
+const ItemPedido = require("../models/ItemPedido")
+
+const cadastrar = async (req,res) =>{
+    const body = req.body
+    try{
+        await ItemPedido.create(body)
+        res.status(200).json({message: "itemPedido cadatrado"})
+    }catch(err){
+        res.status(500).json({error: "Erro ao cadastrar o itemPedido"})
+        console.error("Erro ao cadastrar o itemPedido",err)
+    }
+}
+
+module.exports = { cadastrar }
