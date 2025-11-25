ebabdfdbb8925de628086a307567ad72c5811f84
Mateus Queiroz <mateus_logrado@estudante.sesisenai.org.br>
2025-11-25 07:51:00 -0300

    produto controller

---
+++ controller/produto.controller.js
@@ -0,0 +1,60 @@
+const { UPDATE } = require("sequelize/lib/query-types")
+const Produto = require("../models/Produto")
+
+const cadastrar = async (req,res) =>{
+    const body = req.body
+    try{
+        await Produto.create(body)
+        res.status(200).json({message: "Produto cadatrado"})
+    }catch(err){
+        res.status(500).json({error: "Erro ao cadastrar o produto"})
+        console.error("Erro ao cadastrar o produto",err)
+    }
+}
+
+const listar = async (req,res) =>{
+    try{
+        const produtos = await Produto.findAll()
+        res.status(200).json(produtos)
+    }catch(err){
+        res.status(500).json({error: "Erro ao listar o produto"})
+        console.error("Erro ao listar o produto",err)
+    }
+}
+
+const atualizar = async (req,res) =>{
+    const body = req.body
+    const id = req.params.id
+
+    try{
+        const produto = await Produto.findByPk(id)
+        if(!produto){
+            res.status(404).json({error: "produto não encontrado"})
+        }else{
+            await Produto.update(body, {where: {codProduto: id}})
+            res.status(200).json({message: "Produto atualizado com sucesso"})
+        }
+    }catch(err){
+        res.status(500).json({error: "Erro ao atualizar o produto"})
+        console.error("Erro ao atualizar o produto",err)
+    }
+}
+
+const apagar = async (req,res) =>{
+    const id = req.params.id
+
+    try{
+        const produto = await Produto.findByPk(id)
+        if(!produto){
+            res.status(404).json({error: "produto não encontrado"})
+        }else{
+            await Produto.destroy({where: {codProduto: id}})
+            res.status(200).json({message: "Produto apagado com sucesso"})
+        }
+    }catch(err){
+        res.status(500).json({error: "Erro ao apagar o produto"})
+        console.error("Erro ao apagar o produto",err)
+    }
+}
+
+module.exports = { cadastrar, listar, atualizar, apagar }
--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.use(express.json())
 app.use(cors())
 
 const authMiddleware = require("./middleware/auth.middleware")
-const usuarioController = require("./controller/usuario.controller")
 const authController = require("./controller/auth.controller")
+const usuarioController = require("./controller/usuario.controller")
+const enderecoController = require("./controller/endereco.controller")
+const produtoController = require("./controller/produto.controller")
 
 app.get("/", (req,res)=>{
     res.status(201).json({message: "Aplicação rodando!"})
@@ -27,7 +29,17 @@ app.use(authMiddleware)
 
 app.put("/usuario", usuarioController.atualizar)
 app.delete("/usuario", usuarioController.apagar)
-app.post("/usuario", usuarioController.consultar)
+app.post("/usuario/consultar", usuarioController.consultar)
+
+app.post("/endereco", enderecoController.cadastrar)
+app.get("/endereco", enderecoController.listar)
+app.put("/endereco", enderecoController.atualizar)
+app.delete("/endereco", enderecoController.apagar)
+
+app.post("/eproduto", produtoController.cadastrar)
+app.get("/produtoo", produtoController.listar)
+app.put("/eproduto", produtoController.atualizar)
+app.delete("/eproduto", produtoController.apagar)
 
 conn.sync()
 .then(()=>{
